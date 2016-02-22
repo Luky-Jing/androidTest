0fca16b7113889e8e7e1593bb7a74a625c12c817
android-LPJ <18434361814@163.com>
2016-02-22 10:38:52 +0800

    2.20继续编写上两个工程 优化按钮 2.21设置点击事件 存在BUG 2.22修改BUG 完善计算

--- counter/src/main/java/lpj/nuc/edu/com/androidtest/MainActivity.java
+++ counter/src/main/java/lpj/nuc/edu/com/androidtest/MainActivity.java
@@ -1,13 +1,13 @@
 package lpj.nuc.edu.com.androidtest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-
+public class MainActivity extends Activity implements OnClickListener {
     Button btn_0;
     Button btn_1;
     Button btn_2;
@@ -18,23 +18,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     Button btn_7;
     Button btn_8;
     Button btn_9;
+
     Button btn_point;
+    Button btn_divide;
+    Button btn_multiply;
+    Button btn_minus;
+    Button btn_pluse;
+    Button btn_equal;
+
     Button btn_clear;
     Button btn_del;
-    Button btn_plus;
-    Button btn_minus;
-    Button btn_multiply;
-    Button btn_divide;
-    Button btn_equle;
-    EditText et_input;  //显示输入内容
 
-    boolean clear_flag;
+    EditText et_showview;
+    boolean needclear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         btn_0 = (Button) findViewById(R.id.btn_0);
         btn_1 = (Button) findViewById(R.id.btn_1);
         btn_2 = (Button) findViewById(R.id.btn_2);
@@ -46,14 +47,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btn_8 = (Button) findViewById(R.id.btn_8);
         btn_9 = (Button) findViewById(R.id.btn_9);
         btn_point = (Button) findViewById(R.id.btn_point);
-        btn_plus = (Button) findViewById(R.id.btn_plus);
-        btn_minus = (Button) findViewById(R.id.btn_minus);
-        btn_multiply = (Button) findViewById(R.id.btn_multiply);
         btn_divide = (Button) findViewById(R.id.btn_divide);
-        btn_equle = (Button) findViewById(R.id.btn_equal);
-        //以上是实例化按钮
+        btn_multiply = (Button) findViewById(R.id.btn_multiply);
+        btn_minus = (Button) findViewById(R.id.btn_minus);
+        btn_pluse = (Button) findViewById(R.id.btn_pluse);
+        btn_equal = (Button) findViewById(R.id.btn_equal);
 
-        et_input = (EditText) findViewById(R.id.et_input);  //实例化显示屏
+        btn_clear = (Button) findViewById(R.id.btn_clear);
+        btn_del = (Button) findViewById(R.id.btn_del);
+        et_showview = (EditText) findViewById(R.id.et_showview);
 
         btn_0.setOnClickListener(this);
         btn_1.setOnClickListener(this);
@@ -65,20 +67,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btn_7.setOnClickListener(this);
         btn_8.setOnClickListener(this);
         btn_9.setOnClickListener(this);
-        btn_point.setOnClickListener(this);
-        btn_plus.setOnClickListener(this);
-        btn_minus.setOnClickListener(this);
-        btn_multiply.setOnClickListener(this);
-        btn_divide.setOnClickListener(this);
-        btn_equle.setOnClickListener(this);
-        //以上是设置按钮点击事件
 
+        btn_point.setOnClickListener(this);
+        btn_divide.setOnClickListener(this);
+        btn_multiply.setOnClickListener(this);
+        btn_minus.setOnClickListener(this);
+        btn_pluse.setOnClickListener(this);
+        btn_equal.setOnClickListener(this);
+
+        btn_clear.setOnClickListener(this);
+        btn_del.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        String str = et_input.getText().toString();
-        switch (v.getId()){
+        // TODO Auto-generated method stub
+        String str = et_showview.getText().toString();
+        switch (v.getId()) {
             case R.id.btn_0:
             case R.id.btn_1:
             case R.id.btn_2:
@@ -90,106 +95,104 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.btn_8:
             case R.id.btn_9:
             case R.id.btn_point:
-                if(clear_flag){
-                    clear_flag = false;
+                if (needclear) {
+                    needclear = false;
                     str = "";
-                    et_input.setText("");
+                    et_showview.setText("");
                 }
-                et_input.setText(str + ((Button)v).getText());
+                et_showview.setText(str + ((Button) v).getText());
                 break;
-            case R.id.btn_plus:
+            case R.id.btn_pluse:
             case R.id.btn_minus:
             case R.id.btn_multiply:
             case R.id.btn_divide:
-                if(clear_flag){
-                    clear_flag = false;
+                if (needclear) {
+                    needclear = false;
                     str = "";
-                    et_input.setText("");
+                    et_showview.setText("");
                 }
-                et_input.setText(str + " " + ((Button)v).getText());
-                break;
-            case R.id.btn_clear:
-                clear_flag = false;
-                str = "";
-                et_input.setText("");
-                break;
-            case R.id.btn_del:
-                if(clear_flag){
-                    clear_flag = false;
-                    str = "";
-                    et_input.setText("");
-                }else if(str != null && !str.equals("")){
-                    et_input.setText(str.substring(0, str.length() - 1));
-                }
+                et_showview.setText(str + " " + ((Button) v).getText() + " ");
                 break;
             case R.id.btn_equal:
                 getResult();
                 break;
-            default:
+            case R.id.btn_del:
+                if (needclear) {
+                    needclear = false;
+                    str = "";
+                    et_showview.setText("");
+                } else if (str != null && !str.equals("")) {
+                    et_showview.setText(str.substring(0, str.length() - 1));
+                }
+                break;
+            case R.id.btn_clear:
+                needclear = false;
+                et_showview.setText("");
+                break;
         }
     }
 
-    //运算结果
-    private void getResult(){
-        String exp = et_input.getText().toString();
+    private void getResult() {
+        String exp = et_showview.getText().toString();
         if(exp == null || exp.equals("")){
             return;
         }
-        if(!exp.contains(" ")){
+        if (!exp.contains(" ")){
             return;
         }
-        if(clear_flag){
-            clear_flag = false;
+        if (needclear){
+            needclear = false;
             return;
         }
-        clear_flag = true;
-        double resul = 0;
-        String s1 = exp.substring(0, exp.indexOf(" ")); //运算符前面的字符串
-        String op = exp.substring(exp.indexOf(" ") + 1, exp.indexOf(" ") + 2);  //截取的运算符
-        String s2 = exp.substring(exp.indexOf(" ") + 3);    //运算符后面的字符串
+        needclear = true;
+        double r = 0;
+        String s1 = exp.substring(0, exp.indexOf(" "));
+        String op = exp.substring(exp.indexOf(" ") + 1, exp.indexOf(" ") + 2);
+        String s2 = exp.substring(exp.indexOf(" ") + 3);
         if (!s1.equals("") && !s2.equals("")){
-            double d1 = Double.parseDouble(s1);
-            double d2 = Double.parseDouble(s2);
-            if(op.equals("+")){
-                resul = d1 + d2;
-            }else if (op.equals("-")){
-                resul = d1 - d2;
-            }else if (op.equals("*")){
-                resul = d1 * d2;
-            }else if (op.equals("/")){
-                if (d2 == 0){
-                    resul = 0;
-                }else {
-                    resul = d1 / d2;
+            double arg1 = Double.parseDouble(s1);
+            double arg2 = Double.parseDouble(s2);
+            if (op.equals("+")) {
+                r = arg1 + arg2;
+            } else if (op.equals("-")) {
+                r = arg1 - arg2;
+            } else if (op.equals("*")) {
+                r = arg1 * arg2;
+            } else if (op.equals("/")) {
+                if (arg2 == 0) {
+                    r = 0;
+                } else {
+                    r = arg1 / arg2;
                 }
             }
-            if(!s1.contains(".") && !s2.contains(".") && !op.equals("/")){
-                int r = (int)resul;
-                et_input.setText(r + "");
-            }else {
-                et_input.setText(resul + "");
+            if (!s1.contains(".") && !s2.contains(".") && !op.equals("/")) {
+                int result = (int) r;
+                et_showview.setText(result + "");
+            } else {
+                et_showview.setText(r + "");
             }
-        }else if(!s1.equals("") && s2.equals("")){
-            et_input.setText(exp);
-        }else if (s1.equals("") && !s2.equals("")){
-            double d2 = Double.parseDouble(s2);
-            if(op.equals("+")){
-                resul = 0 + d2;
-            }else if (op.equals("-")){
-                resul = 0 - d2;
-            }else if (op.equals("*")){
-                resul = 0;
-            }else if (op.equals("/")){
-               resul = 0;
+        } else if (!s1.equals("") && s2.equals("")){
+            et_showview.setText(exp);
+        } else if (s1.equals("") && !s2.equals("")){
+            double arg2 = Double.parseDouble(s2);
+            if (op.equals("+")) {
+                r = 0 + arg2;
+            } else if (op.equals("-")) {
+                r = 0 - arg2;
+            } else if (op.equals("*")) {
+                r = 0;
+            } else if (op.equals("/")) {
+                r = 0;
             }
-            if(!s2.contains(".")){
-                int r = (int)resul;
-                et_input.setText(r + "");
-            }else {
-                et_input.setText(resul + "");
+            if (!s1.contains(".") && !s2.contains(".")) {
+                int result = (int) r;
+                et_showview.setText(result + "");
+            } else {
+                et_showview.setText(r + "");
             }
-        }else if (s1.equals("") && s2.equals("")){
-            et_input.setText("");
+        } else if (s1.equals("") && s2.equals("")){
+            et_showview.setText("");
         }
+
     }
 }
